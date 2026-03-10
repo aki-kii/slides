37b903ecc3c937d93285fd90980a497bdcf5da43
aki-kii <impeeeeedance@gmail.com>
2026-03-11 08:20:37 +0900

    write: TitledSectionを追記、表現を修正

---
+++ packages/slides/cdk-day1-experiment/components/TitledSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-baseline gap-4 mb-4">
+    <h2 class="!m-0">{{ title }}</h2>
+    <span>{{ description }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description: string
+}>()
+</script>
